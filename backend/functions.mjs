a2c17f3160d7505810dc6b74ed3cd5bb87215974
ahadsts9901 <ahadsts990@gmail.com>
2024-08-05 15:05:48 +0500

    add-product-api

--- backend/functions.mjs
+++ backend/functions.mjs
@@ -1,6 +1,53 @@
 import "dotenv/config"
 import jwt from "jsonwebtoken"
 import { initialSessionInDays } from "./core.mjs"
+import multer, { diskStorage } from 'multer'
+import { v2 as cloudinary } from 'cloudinary'
+import fs from "fs"
+
+const storageConfig = diskStorage({
+    destination: './uploads/',
+    filename: function (req, file, cb) {
+        console.log("mul-file: ", file);
+        cb(null, `image-${new Date().getTime()}-${file.originalname}`)
+    }
+})
+
+export const upload = multer({ storage: storageConfig })
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export const uploadOnCloudinary = async (localFilePath) => {
+
+    try {
+
+        if (!localFilePath) {
+            console.log("no file");
+            return null
+        }
+
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto",
+        })
+
+        fs.unlink(localFilePath, (unlinkError) => {
+            if (unlinkError) {
+                console.error("Error removing local file:", unlinkError);
+            }
+        });
+
+        return response?.url;
+
+    } catch (error) {
+        console.log(error);
+        fs.unlinkSync(localFilePath)
+        return null;
+    }
+}
 
 export const issueLoginToken = async (user, res) => {
 
--- backend/routes/products/main.mjs
+++ backend/routes/products/main.mjs
@@ -1,5 +1,82 @@
 import { Router } from "express"
+import { Product } from "../../models/main.mjs"
+import { upload, uploadOnCloudinary } from "../../functions.mjs"
 
 const router = Router()
+
+router.get("/products", async (req, res, next) => {
+
+    try {
+
+        const products = await Product.findAll()
+
+        res.send({
+            message: "products fetched",
+            data: products
+        })
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).send({
+            message: "internal server error",
+            error: error?.message
+        })
+    }
+
+})
+
+router.post("/products", upload.any(), async (req, res, next) => {
+
+    const { title, description, price } = req?.body
+
+    if (!title || title?.trim() === "") {
+        return res.status(400).send({
+            message: "title is required"
+        })
+    }
+
+    if (!description || description?.trim() === "") {
+        return res.status(400).send({
+            message: "description is required"
+        })
+    }
+
+    if (!price) {
+        return res.status(400).send({
+            message: "price is required"
+        })
+    }
+
+    if (!req?.files || !req?.files?.length || !req?.files[0]) {
+        return res.status(400).send({
+            message: "image is required"
+        })
+    }
+
+    try {
+
+        const imageUrl = await uploadOnCloudinary(req?.files[0]?.path)
+
+        const productResponse = await Product.create({
+            title: title,
+            description: description,
+            price: price,
+            imageUrl: imageUrl
+        })
+
+        res.send({
+            message: "product added successfully",
+            data: productResponse?.dataValues
+        })
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).send({
+            message: "internal server error",
+            error: error?.message
+        })
+    }
+
+})
 
 export default router
