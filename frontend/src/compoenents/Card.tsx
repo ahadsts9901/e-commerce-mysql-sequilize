120c070ee6025cda3a9fc930f932a75fc5dc8c8c
ahadsts9901 <ahadsts990@gmail.com>
2024-08-07 12:27:12 +0500

    admin

--- frontend/src/compoenents/Card.tsx
+++ frontend/src/compoenents/Card.tsx
@@ -8,6 +8,77 @@ import { useNavigate } from 'react-router-dom';
 import { IoMdCart } from "react-icons/io";
 import { MdArrowOutward } from "react-icons/md";
 import { useSelector } from 'react-redux'
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import React, { useState } from 'react';
+import ModeIcon from '@mui/icons-material/Mode';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+const DropMenu = ({ productId }: any) => {
+
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+
+    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const options = [
+        { label: "Edit", icon: <ModeIcon sx={{ width: "0.8em", height: "0.8em", marginRight: "0.2em" }} />, fun: (id: string | number) => editProduct(id) },
+        { label: "Delete", icon: <DeleteIcon sx={{ width: "0.8em", height: "0.8em", marginRight: "0.2em" }} />, fun: (id: string | number) => deleteProduct(id) },
+    ]
+
+    const deleteProduct = async (id: string | number) => {
+        console.log("delete", id)
+    }
+
+    const editProduct = async (id: string | number) => {
+        console.log("edit", id)
+    }
+
+    return (
+        <div>
+            <IconButton
+                aria-label="more"
+                id="long-button"
+                aria-controls={open ? 'long-menu' : undefined}
+                aria-expanded={open ? 'true' : undefined}
+                aria-haspopup="true"
+                onClick={handleClick}
+            >
+                <MoreVertIcon />
+            </IconButton>
+            <Menu
+                id="long-menu"
+                MenuListProps={{
+                    'aria-labelledby': 'long-button',
+                }}
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+            >
+                {options?.map((option: any, i: number) => (
+                    <div key={i}>
+                        <MenuItem key={option} onClick={() => {
+                            handleClose()
+                            option?.fun(productId)
+                        }}>
+                            {option.icon}
+                            {option.label}
+                        </MenuItem>
+                    </div>
+                ))}
+            </Menu>
+        </div>
+    )
+
+}
 
 const _Card = ({ title, description, price, image, id }: any) => {
 
@@ -45,7 +116,10 @@ const _Card = ({ title, description, price, image, id }: any) => {
                             <Button size="small" sx={{ height: "2.5em" }} color='primary' variant='outlined'>
                                 <IoMdCart style={{ color: "#364B63", fontSize: "1.5em", marginRight: "0.5em" }} />
                                 Add To Cart
-                            </Button> : null
+                            </Button> :
+                            <div className='drop-menu-parent'>
+                                <DropMenu productId={id} />
+                            </div>
                     }
                 </CardActions>
             </Card>
